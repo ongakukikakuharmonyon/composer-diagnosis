9f732c9365227b8bb0ac6bc679d2560265ddc8bf
ongakukikakuharmonyon <ongakukikaku.harmonyon@gmail.com>
2025-06-06 17:07:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def show_result_screen():
     st.header(f'あなたは **{result["name"]}タイプ** - {result["type"]} です！')
     
     # 【修正箇所】result['image']からファイル名を読み込み、キャプションを追加
-    st.image(result['image'], caption=f"あなたのタイプ：{result['name']}", use_column_width='auto')
+    st.image(result['image'], caption=f"あなたのタイプ：{result['name']}", use_column_width=True)
     
     st.markdown(f"--- \n ### {result['type']} の特徴")
     st.write(result['text'])
